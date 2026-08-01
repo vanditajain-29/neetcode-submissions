b0bd293dd84f1397a32ed3ef0330393426a69a6e
Vandita Jain <vandy2901@gmail.com>
2026-08-01 23:09:05 +0530

    Update: longest-common-subsequence - submission-0

--- Data Structures & Algorithms/longest-common-subsequence/submission-0.cpp
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
-        for (int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                if (text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n));
+        for(int i=0; i<n; i++){
+            dp[0][i]=1;
+        }
+        for(int j=0; j<m; j++){
+            dp[j][0]=1;
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-
-        return dp[n][m];
+        return dp[m-1][n-1];
     }
 };
