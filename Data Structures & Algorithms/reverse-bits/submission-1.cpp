5f6af8ebe5e682413b53f286d6ffd6aaa2b7469a
Vandita Jain <vandy2901@gmail.com>
2026-08-02 18:42:16 +0530

    Update: reverse-bits - submission-1

--- Data Structures & Algorithms/reverse-bits/submission-1.cpp
+++ Data Structures & Algorithms/reverse-bits/submission-1.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        vector<int> dp(n+1);
-        for(int i=0; i<=n; i++){
-            dp[i]=dp[i>>1]+(i&1);
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res=0;
+        for (int i=0; i<32; i++){
+            uint32_t bit= (n>>i) & 1;
+            res+=(bit<<(31-i));
         }
-        return dp;
+        return res;
     }
 };
